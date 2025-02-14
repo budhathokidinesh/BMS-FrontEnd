b5e4a201302e8a82b72b5baddef71bcfc927d98e
Dinesh Budhathoki <physmarika@gmail.com>
2025-02-14 11:37:02 +0800

    Custom hook-use form is created

--- src/pages/auth/SignupPage.jsx
+++ src/pages/auth/SignupPage.jsx
@@ -3,15 +3,28 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { CustomInput } from "../../components/customInput/CustomInput";
 import { signupInputs } from "../../assets/customInputs/userSignupInputs";
+import useForm from "../../hooks/useForm.js";
 
+const initialState = {};
 const SignupPage = () => {
+  const { form, setForm, handleOnChange } = useForm(initialState);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
     <div className="d-flex justify-content-center">
-      <Form style={{ width: "450px" }} className="card p-3 mt-4 mb-5 shadow-lg">
+      <Form
+        style={{ width: "450px" }}
+        onSubmit={handleOnSubmit}
+        className="card p-3 mt-4 mb-5 shadow-lg"
+      >
         <h3>Join our Book Shop</h3>
         <hr />
         {signupInputs.map((input) => (
-          <CustomInput key={input.name} {...input} />
+          <CustomInput key={input.name} {...input} onChange={handleOnChange} />
         ))}
 
         <Button variant="primary" type="submit">
